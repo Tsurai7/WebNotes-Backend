b344191974063927d4f461346dc926838bdc47db
Nikita <tsurai7walkin@gmail.com>
2023-10-20 09:38:59 +0300

    Add project files.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set;}
+    }
+}
