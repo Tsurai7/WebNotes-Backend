fb41e2ec5eba5d591bcb3720a6b9408f6477be45
Nikita <tsurai7walkin@gmail.com>
2023-10-22 22:24:17 +0300

    Added mapping

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -0,0 +1,30 @@
+﻿using Notes.Application.Common.Mappings;
+using Notes.Domain;
+using AutoMapper;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class NoteDetailsVm : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set;}
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteDetailsVm>()
+                .ForMember(noteVm => noteVm.Title,
+                opt => opt.MapFrom(note => note.Title))
+                .ForMember(noteVm => noteVm.Description,
+                opt => opt.MapFrom(note => note.Description))
+                .ForMember(noteVm => noteVm.Id,
+                opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteVm => noteVm.CreatedAt,
+                opt => opt.MapFrom(note => note.CreatedAt))
+                .ForMember(noteVm => noteVm.UpdatedAt,
+                opt => opt.MapFrom(note => note.UpdatedAt));
+        }
+    }
+}
